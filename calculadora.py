c4b8bb1d2e8ab1a8b6fb20f5a3d0b4ccef623a90
Francisboia <francisco.de.almeida@escola.pr.gov.br>
2025-05-09 14:24:30 -0300

    Create calculadora.py
    https://onlinegdb.com/wtE8veXaj

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+# Solicita ao usuário para inserir dois números
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma
+soma = numero1 + numero2
+
+# Imprime o resultado
+print("A soma de {} e {} é: {}".format(numero1, numero2, soma))
+
