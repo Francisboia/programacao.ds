7449ac6c96110d8b9f8645c0a04ba45928e423e5
Francisboia <francisco.de.almeida@escola.pr.gov.br>
2025-06-13 14:07:55 -0300

    Create faixa_etaria.js

---
+++ faixa_etaria.js
@@ -0,0 +1,18 @@
+
+let idade = 28;
+let categoria;
+
+if (idade < 12) {
+  categoria = "Criança";
+} else if (idade >= 12 && idade < 18) {
+  categoria = "Adolescente";
+} else if (idade >= 18 && idade < 30) {
+  categoria = "jovem Adulto";
+} else if (idade >= 30 && idade < 60) {
+  categoria = "jovem Adulto";
+} else {
+  categoria = "Idoso";
+}
+
+console.log("A pessoa é classificada como: " + categoria);
+
