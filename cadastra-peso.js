ead0e96fabb77e90d921a5050c8602ac0543320c
Francisboia <francisco.de.almeida@escola.pr.gov.br>
2025-06-25 14:29:21 -0300

    Create cadastra-peso.js

---
+++ cadastra-peso.js
@@ -0,0 +1,8 @@
+// Solicita o nome do usuário
+let nome = prompt("Digite seu nome:");
+
+// Solicita a idade do usuário e converte para número
+let peso = parseInt(prompt("Digite seu peso:"));
+
+// Exibe as informações coletadas
+console.log(`Olá ${nome}, você tem ${peso} kg.`);
