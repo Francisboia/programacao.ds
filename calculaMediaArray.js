8a91cb92b258b870aa39c37503276ccd5971cc88
Francisboia <francisco.de.almeida@escola.pr.gov.br>
2025-11-05 15:03:05 -0300

    Add function to calculate average from an array

---
+++ calculaMediaArray.js
@@ -0,0 +1,13 @@
+// Exercício 7 e 8 - Média com array e laço for...of
+
+function calcularMediaArray(notas) {
+  let soma = 0;
+  for (let nota of notas) {
+    soma += nota;
+  }
+  return soma / notas.length;
+}
+
+const notas = [10, 9, 8.33];
+const media = calcularMediaArray(notas);
+console.log(`A média das notas é: ${media.toFixed(2)}`);
