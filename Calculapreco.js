90c55f77708b5991350c70f8b8d8e1e85b877633
Francisboia <francisco.de.almeida@escola.pr.gov.br>
2025-11-19 14:48:22 -0300

    Add calcularPrecoFinal function for price calculation
    Function to calculate final price with discount and log results.

---
+++ Calculapreco.js
@@ -0,0 +1,23 @@
+function calcularPrecoFinal(produto) {
+  const desconto = produto.desconto ?? 0;
+
+  const precoFinal = produto.preco - (produto.preco * desconto / 100);
+
+  console.log(`Preço final de ${produto.nome}: R$ ${precoFinal.toFixed(2)}`);
+  console.log(`Preço final de ${produto.nome}: R$ ${produto.preco}`);
+
+}
+
+// Exemplo de uso
+
+const produto1 = {
+
+  nome: "Fone de ouvido",
+
+  preco: 200,
+
+  desconto: 25
+
+};
+
+calcularPrecoFinal(produto1);
